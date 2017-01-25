92e97af4018ae4726072a08787f2c7ce804d1c56
Kabelo Moiloa <moiloakabelo@gmail.com>
2017-01-25 13:53:51 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  for row in 0..2
+    for col in 0..1
+      print " #{board[row * 3 + col]} |"
+    end
+    puts " #{board[row * 3 + 2]} "
+    puts "-----------"
+  end
+end
